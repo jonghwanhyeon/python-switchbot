4a35464f2eb897ad78af19476d0d1167497f2d17
Jonghwan Hyeon <hyeon0145@gmail.com>
2021-08-24 21:50:20 +0900

    Extract mapping

--- switchbot/devices.py
+++ switchbot/devices.py
@@ -6,6 +6,26 @@ import humps
 
 from switchbot.client import SwitchBotClient
 
+status_key_mapping = {
+    'power': 'power',
+    'humidity': 'humidity',
+    'temperature': 'temperature',
+    'nebulization_efficiency': 'nebulization_efficiency',
+    'auto': 'auto',
+    'child_lock': 'safety_lock',
+    'sound': 'sound',
+    'calibrate': 'calibrate',
+    'group': 'grouped',
+    'moving': 'moving',
+    'slide_position': 'slide_position',
+    'mode': 'mode',
+    'speed': 'speed',
+    'speed': 'speed',
+    'shaking': 'oscillating',
+    'shake_center': 'oscillating_center',
+    'shake_range': 'oscillating_range'
+}
+
 
 class Device:
     device_type_for: ClassVar[Optional[str]] = None
@@ -31,28 +51,10 @@ class Device:
         return device_cls(client, id=id, **extra)
 
     def status(self) -> Dict[str, Any]:
-        mapping = {'power': 'power',
-                   'humidity': 'humidity',
-                   'temperature': 'temperature',
-                   'nebulization_efficiency': 'nebulization_efficiency',
-                   'auto': 'auto',
-                   'child_lock': 'safety_lock',
-                   'sound': 'sound',
-                   'calibrate': 'calibrate',
-                   'group': 'grouped',
-                   'moving': 'moving',
-                   'slide_position': 'slide_position',
-                   'mode': 'mode',
-                   'speed': 'speed',
-                   'speed': 'speed',
-                   'shaking': 'oscillating',
-                   'shake_center': 'oscillating_center',
-                   'shake_range': 'oscillating_range'}
-
         response = self.client.get(f'devices/{self.id}/status')
-        return {mapping[key]: value
+        return {status_key_mapping[key]: value
                 for key, value in response['body'].items()
-                if key in mapping}
+                if key in status_key_mapping}
 
     def command(self, action: str, parameter: Optional[str] = None):
         parameter = 'default' if parameter is None else parameter
