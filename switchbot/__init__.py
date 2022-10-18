6b900f331c1a9d7d8a2075ea84a28f6f31248a96
Jonghwan Hyeon <hyeon0145@gmail.com>
2022-10-18 18:55:17 +0900

    Format documents

--- switchbot/__init__.py
+++ switchbot/__init__.py
@@ -6,21 +6,25 @@ from switchbot.client import SwitchBotClient
 
 
 class SwitchBot:
-    def __init__(self, token: str, secret: str, nonce: str = ''):
+    def __init__(self, token: str, secret: str, nonce: str = ""):
         self.client = SwitchBotClient(token, secret, nonce)
 
     def devices(self) -> List[Device]:
-        response = self.client.get('devices')
-        return [self.device(device['device_id'], **device)
-                for device in response['body']['device_list']]
+        response = self.client.get("devices")
+        return [
+            self.device(device["device_id"], **device)
+            for device in response["body"]["device_list"]
+        ]
 
     def device(self, id: str, **extra) -> Device:
         return Device.create(client=self.client, id=id, **extra)
 
     def remotes(self) -> List[Remote]:
-        response = self.client.get('devices')
-        return [self.remote(remote['device_id'], **remote)
-                for remote in response['body']['infrared_remote_list']]
+        response = self.client.get("devices")
+        return [
+            self.remote(remote["device_id"], **remote)
+            for remote in response["body"]["infrared_remote_list"]
+        ]
 
     def remote(self, id: str, **extra) -> Remote:
         return Remote.create(client=self.client, id=id, **extra)
--- switchbot/devices.py
+++ switchbot/devices.py
@@ -7,25 +7,25 @@ import humps
 from switchbot.client import SwitchBotClient
 
 status_key_mapping = {
-    'power': 'power',
-    'humidity': 'humidity',
-    'temperature': 'temperature',
-    'nebulization_efficiency': 'nebulization_efficiency',
-    'auto': 'auto',
-    'child_lock': 'safety_lock',
-    'sound': 'sound',
-    'calibrate': 'calibrate',
-    'group': 'grouped',
-    'moving': 'moving',
-    'slide_position': 'slide_position',
-    'mode': 'mode',
-    'speed': 'speed',
-    'speed': 'speed',
-    'shaking': 'oscillating',
-    'shake_center': 'oscillating_center',
-    'shake_range': 'oscillating_range',
-    'lock_state': 'lock_state',
-    'door_state': 'door_state'
+    "power": "power",
+    "humidity": "humidity",
+    "temperature": "temperature",
+    "nebulization_efficiency": "nebulization_efficiency",
+    "auto": "auto",
+    "child_lock": "safety_lock",
+    "sound": "sound",
+    "calibrate": "calibrate",
+    "group": "grouped",
+    "moving": "moving",
+    "slide_position": "slide_position",
+    "mode": "mode",
+    "speed": "speed",
+    "speed": "speed",
+    "shaking": "oscillating",
+    "shake_center": "oscillating_center",
+    "shake_range": "oscillating_range",
+    "lock_state": "lock_state",
+    "door_state": "door_state",
 }
 
 
@@ -37,10 +37,10 @@ class Device:
         self.client = client
 
         self.id: str = id
-        self.name: str = extra.get('device_name')
-        self.type: str = extra.get('device_type')
-        self.cloud_enabled: bool = extra.get('enable_cloud_service')
-        self.hub_id: str = extra.get('hub_device_id')
+        self.name: str = extra.get("device_name")
+        self.type: str = extra.get("device_type")
+        self.cloud_enabled: bool = extra.get("enable_cloud_service")
+        self.hub_id: str = extra.get("hub_device_id")
 
     def __init_subclass__(cls):
         if cls.device_type_for is not None:
@@ -48,73 +48,79 @@ class Device:
 
     @classmethod
     def create(cls, client: SwitchBotClient, id: str, **extra):
-        device_type = extra.get('device_type')
+        device_type = extra.get("device_type")
         device_cls = cls.specialized_cls.get(device_type, Device)
         return device_cls(client, id=id, **extra)
 
     def status(self) -> Dict[str, Any]:
-        response = self.client.get(f'devices/{self.id}/status')
-        return {status_key_mapping[key]: value
-                for key, value in response['body'].items()
-                if key in status_key_mapping}
+        response = self.client.get(f"devices/{self.id}/status")
+        return {
+            status_key_mapping[key]: value
+            for key, value in response["body"].items()
+            if key in status_key_mapping
+        }
 
     def command(self, action: str, parameter: Optional[str] = None):
-        parameter = 'default' if parameter is None else parameter
-        payload = humps.camelize({
-            'command_type': 'command',
-            'command': humps.camelize(action),
-            'parameter': parameter})
+        parameter = "default" if parameter is None else parameter
+        payload = humps.camelize(
+            {
+                "command_type": "command",
+                "command": humps.camelize(action),
+                "parameter": parameter,
+            }
+        )
 
-        self.client.post(f'devices/{self.id}/commands', json=payload)
+        self.client.post(f"devices/{self.id}/commands", json=payload)
 
     def __repr__(self):
-        name = 'Device' if self.type is None else self.type
-        name = name.replace(' ', '')
-        return f'{name}(id={self.id})'
+        name = "Device" if self.type is None else self.type
+        name = name.replace(" ", "")
+        return f"{name}(id={self.id})"
 
 
 class Bot(Device):
-    device_type_for = 'Bot'
+    device_type_for = "Bot"
 
     def turn(self, state: str):
         state = state.lower()
-        assert state in ('on', 'off')
-        self.command(f'turn_{state}')
+        assert state in ("on", "off")
+        self.command(f"turn_{state}")
 
     def press(self):
-        self.command('press')
+        self.command("press")
 
     def toggle(self):
-        state = self.status()['power']
-        self.turn('on' if state == 'off' else 'off')
+        state = self.status()["power"]
+        self.turn("on" if state == "off" else "off")
 
 
 class Curtain(Device):
-    device_type_for = 'Curtain'
+    device_type_for = "Curtain"
 
     def __init__(self, client: SwitchBotClient, id: str, **extra):
         super().__init__(self, client, id, **extra)
 
-        self.curtain_ids: List[str] = extra.get('curtain_devices_ids')
-        self.calibrated: bool = extra.get('calibrate')
-        self.grouped: bool = extra.get('group')
-        self.master: bool = extra.get('master')
-        self.open_direction: str = extra.get('open_direction')
+        self.curtain_ids: List[str] = extra.get("curtain_devices_ids")
+        self.calibrated: bool = extra.get("calibrate")
+        self.grouped: bool = extra.get("group")
+        self.master: bool = extra.get("master")
+        self.open_direction: str = extra.get("open_direction")
+
 
 class Lock(Device):
-    device_type_for = 'Smart Lock'
+    device_type_for = "Smart Lock"
 
     def lock(self):
-        self.command('lock')
+        self.command("lock")
 
     def unlock(self):
-        self.command('unlock')
+        self.command("unlock")
 
     def toggle(self):
-        state = self.status()['lock_state']
-        assert state in ('unlocked', 'locked')
+        state = self.status()["lock_state"]
+        assert state in ("unlocked", "locked")
 
-        if state == 'unlocked':
+        if state == "unlocked":
             self.lock()
         else:
             self.unlock()
