4f8d93a3d63bfddf1081c2bc589fdf7c05aeba37
Jonghwan Hyeon <hyeon0145@gmail.com>
2022-10-18 19:05:47 +0900

    Version 2.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as input_file:
 
 setup(
     name='python-switchbot',
-    version='2.2.1',
+    version='2.2.2',
     description='A Python library to control SwitchBot devices connected to SwitchBot Hub',
     long_description=long_description,
     long_description_content_type='text/markdown',
