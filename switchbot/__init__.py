667316022c60b00961fe22939e352057a78456c2
Jonghwan Hyeon <hyeon0145@gmail.com>
2022-10-18 19:04:52 +0900

    Resolve #14

--- switchbot/__init__.py
+++ switchbot/__init__.py
@@ -12,19 +12,29 @@ class SwitchBot:
     def devices(self) -> List[Device]:
         response = self.client.get("devices")
         return [
-            self.device(device["device_id"], **device)
+            Device.create(client=self.client, id=device["device_id"], **device)
             for device in response["body"]["device_list"]
         ]
 
-    def device(self, id: str, **extra) -> Device:
-        return Device.create(client=self.client, id=id, **extra)
+    def device(self, id: str) -> Device:
+        # Currently, SwitchBot API does not support to retrieve device_name,
+        # enable_cloud_service and hub_device_id without getting all device list
+        # Therefore, for backward compatibility reason,
+        # we query all devices first, then return the matching device
+        for device in self.devices():
+            if device.id == id:
+                return device
+        raise ValueError(f"Unknown device {id}")
 
     def remotes(self) -> List[Remote]:
         response = self.client.get("devices")
         return [
-            self.remote(remote["device_id"], **remote)
+            Remote.create(client=self.client, id=remote["device_id"], **remote)
             for remote in response["body"]["infrared_remote_list"]
         ]
 
-    def remote(self, id: str, **extra) -> Remote:
-        return Remote.create(client=self.client, id=id, **extra)
+    def remote(self, id: str) -> Remote:
+        for remote in self.remotes():
+            if remote.id == id:
+                return remote
+        raise ValueError(f"Unknown remote {id}")
