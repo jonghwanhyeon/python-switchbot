cb7767194c94ee42b6bed7564ebfc33d71a735bf
Jonghwan Hyeon <hyeon0145@gmail.com>
2022-12-18 14:47:24 +0900

    Update metadata

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open('README.md', 'r') as input_file:
-    long_description = input_file.read()
-
-setup(
-    name='python-switchbot',
-    version='2.2.3',
-    description='A Python library to control SwitchBot devices connected to SwitchBot Hub',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/jonghwanhyeon/python-switchbot',
-    author='Jonghwan Hyeon',
-    author_email='hyeon0145@gmail.com',
-    license='MIT',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Home Automation',
-    ],
-    keywords='switchbot',
-    packages=find_packages(),
-    install_requires=['requests', 'pyhumps'],
-)
+setup()
